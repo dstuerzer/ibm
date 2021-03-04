06646ad68a19c8ebb3ca725a39f92e313a62cdfc
Dominik Stuerzer <dominik.stuerzer@gmail.com>
2021-03-04 20:16:21 +0000

    nice code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def printu(u):
  #   return (i % N_x) * h, (j % N_y) * h
     
 # set up grid
-h = 0.2
+h = 0.05
 x_max = 5
 y_max = 4
 
@@ -23,9 +23,9 @@ h = x_max / J
 K = int(y_max / h)
 
 # set up boundary
-N_theta = 100
+N_theta = 400
 d_theta = 2 * np.pi / N_theta
-X = np.array([np.array([np.cos(s * d_theta) + J * h/2, np.sin(s * d_theta) + K * h /2]) for s in range(N_theta)])
+X = np.array([np.array([1.1*np.cos(s * d_theta) + J * h/2, np.sin(s * d_theta) + K * h /2]) for s in range(N_theta)])
 
 
 
@@ -38,25 +38,33 @@ print(np.linalg.norm(op.Div(u0, h)))
 # parameters
 
 _tension_K = 1
-_mu = 0.1
+_mu = 0.01
 _rho = 1
-dt = 0.05
+dt = 0.02
 fig = plt.figure()
 plt.scatter(X[:, 0], X[:, 1])
 plt.show()
 t = 0
-for ct in range(100):
+
+_x = h * np.array(range(J))
+_y = h * np.array(range(K))
+xy = np.meshgrid(_x, _y)
+
+for ct in range(500):
     print(ct)
     X, u2 = st.RK(X, u0, dt, h, K, J, _tension_K, d_theta, N_theta, _rho, _mu)
     t += dt
     fig.clear()
-    plt.pcolormesh(h * np.array(range(J)), h * np.array(range(K)), np.linalg.norm(u2, axis=0).T)
+    plt.pcolormesh(_x, _y, np.linalg.norm(u2, axis=0).T)
+    
+    
+    plt.quiver(xy[0].T[ ::3, ::3], xy[1].T[ ::3, ::3], u2[0, ::3, ::3], u2[1,  ::3, ::3], scale=1/h)
     #plt.colorbar()
-    plt.scatter(X[:, 0], X[:, 1] , s=1, color = 'black')
+    plt.scatter(X[:, 0], X[:, 1] , s=2, color = 'black')
     plt.draw()
     plt.pause(0.1)
     u0 = np.copy(u2)
-    time.sleep(3)
+   # time.sleep(3)
 plt.show() 
     
                 
