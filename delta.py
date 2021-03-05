16ca2e8042a817c690b2ade9c24fb1f6e99e779c
Dominik Stuerzer <dominik.stuerzer@gmail.com>
2021-03-05 21:40:48 +0000

    actually remove old functions

--- delta.py
+++ delta.py
@@ -1,25 +1,5 @@
 import numpy as np
 
-def fun01(r):
-    return (3 - 2 * r + np.sqrt(1 + 4 * r - 4 * r * r)) / 8.0
-
-def dlta(r):
-    if r < -2.0:
-        return 0
-    elif r < -1:
-        return 0.5 - fun01(r + 2)
-    elif r < 0:
-        return (4 * r + 2) / 8 + fun01(r + 1)
-    elif r < 1:
-        return fun01(r)
-    elif r < 2:
-        return (10 - 4 * r) / 8 - fun01(r - 1)
-    else:
-        return 0
-
-def delta_h(x,h):
-    return dlta(x / h) / h
-
 def delta_x(rr, h):
     r = rr / h
     q = (3 - 2 * r + np.sqrt(1 + 4 * r - 4 * r * r)) / 8
--- stepper.py
+++ stepper.py
@@ -1,6 +1,6 @@
 import numpy as np
 import operators as op
-from delta import delta_x, delta_y, delta_h
+from delta import delta_x, delta_y
 import inverter as inv
 from matplotlib import pyplot as plt
 from time import time
@@ -18,19 +18,6 @@ def first_integration(UV, XY, h, K, J):
     uy_local = np.take(np.take(UV[1, ...], jts, axis=0), ks, axis=1)
     return np.array([np.sum(ux_local * phi), np.sum(uy_local * phi)])
 
-
-def first_integration_old(UV, XY, h, K, J):
-    # UV is full 2 x J x K velocity
-    # XY is X(s)^n, a 2-vector
-    j0, k0 = int(XY[0]/h), int(XY[1]/h)
-    integral = np.zeros((2,))
-    for j in range(j0-3, j0+4):
-        for k in range(k0 - 3, k0+4):
-            _j, _k = j % J, k % K  # <- for u
-            xj, yk = j * h, k * h  # might be "out of bounds"
-            integral += UV[:, _j, _k] * delta_h(xj - XY[0], h) * delta_h(yk - XY[1], h)
-
-    return integral
 
 def second_integration(FF, XY, h, J, K, d_theta):
     # FF == F1
@@ -52,43 +39,16 @@ def second_integration(FF, XY, h, J, K, d_theta):
     return f1
 
 
-def second_integration_old(FF, XY, h, J, K, d_theta):
-    # FF == F1
-    # XY == X1, our curve
-    f1 = np.zeros((2, J, K))
-    for s in range(FF.shape[0]):
-        j0, k0 = int(XY[s, 0]/h), int(XY[s, 1]/h)
-        for j in range(j0-3, j0+4):
-            for k in range(k0 - 3, k0+4):
-                _j, _k = j % J, k % K  # <- for u
-                xj, yk = j * h, k * h
-                f1[:, _j, _k] += FF[s, :] * delta_h(xj - XY[s, 0], h) * delta_h(yk - XY[s, 1], h) * d_theta
-    return f1
-
-
 def RK(X, u0, dt, h, K, J, _tension_K, d_theta, N_theta, _rho, _mu):
 
     X1 = X.copy()
     for s in range(N_theta):
         X1[s, :] += dt * h * h * first_integration(u0, X[s, :], h, K, J) * 0.5
 
-    # >>>>>>>>>>>
- #   X1_old = X.copy()
-  #  for s in range(N_theta):
-   #     X1_old[s, :] += dt * h * h * first_integration_old(u0, X[s, :], h, K, J) * 0.5
-
-   # print(np.linalg.norm(X1 - X1_old))
-    # <<<<<<<<<<<<<
 
     F1 = _tension_K/(d_theta * d_theta) * (np.roll(X1, 1, axis=0) + np.roll(X1, -1, axis=0) - 2 * X1)
 
     f1 = second_integration(F1, X1, h, J, K, d_theta)
-
-    # >>>>>>>>>
- #   f1_old = second_integration_old(F1, X1, h, J, K, d_theta)
-  #
-   # print(np.linalg.norm(f1 - f1_old))
-    # <<<<<<<<<<
 
     w1 = u0 - dt / 2 * op.Suu(u0, h) + dt / (2 * _rho) * f1
 
